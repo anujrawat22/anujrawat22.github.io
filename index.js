743e108c2ea311b4107e0217af0f56609cde642f
anujrawat22 <anuj22rawat20@gmail.com>
2023-02-06 22:38:25 +0530

    .

--- index.js
+++ index.js
@@ -10,3 +10,9 @@
          return fetch(`https://your-proxy.com/github?user=${username}`)
        }
     }).then(r => r.text())
+
+
+    function download(){
+      
+window.open("https://drive.google.com/file/d/1XUASBk9SaAmFSgpU1H22Gp4EhX8CZuLA/view")
+    }
